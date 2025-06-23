94ec187d96be45ccbc0c94d7ce27d70057b86ca7
Omar <omar1752003@gmail.com>
2025-06-23 21:16:03 +0300

    update DashboardLayout to use Outlet for nested routing

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,4 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function DashboardLayout() {
-  return <h1>DashboardLayout</h1>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 export default DashboardLayout;
